4f11117339e33c4ce24f6fbd732850bb60247337
geethu <geethukp004@gmail.com>
2025-03-12 14:49:26 +0530

    create punch in component

---
+++ src/app/pages/punch-in/punch-in.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PunchInDialogComponent } from "../../punch-in-dialog/punch-in-dialog.component";
+@Component({
+  selector: 'app-punch-in',
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    PunchInDialogComponent
+],
+  standalone:true,
+  templateUrl: './punch-in.component.html',
+  styleUrl: './punch-in.component.scss'
+})
+export class DialogAnimationsExampleDialog {
+
+}
