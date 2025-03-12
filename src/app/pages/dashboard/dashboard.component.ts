566252f030a3d43cf3b1e47b920458703d28544b
geethu <geethukp004@gmail.com>
2025-03-12 14:49:02 +0530

    update dashboard with punch in open dialog

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard/dashboard.service';
 import { CookieService} from 'ngx-cookie-service';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog'; 
+import { MatButtonModule} from '@angular/material/button';
+import { DialogAnimationsExampleDialog } from '../punch-in/punch-in.component';
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, 
+            MatButtonModule,
+            DialogAnimationsExampleDialog         
+           ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  standalone:true,
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent  {
   userDetails:any;
-  constructor(private service:DashboardService,private cookieservice:CookieService,){}
+  constructor(private service:DashboardService,
+    private cookieservice:CookieService,
+    private dialog:MatDialog){}
 
   ngOnInit(): void {
-this.getUser();
+    this.getUser();
 
   }
   getUser(){
     const userId= this.cookieservice.get('userId');
-    console.log("userId",userId);
-
     this.service.getUserDetails(userId).subscribe((res: any)=>{
       this.userDetails = res;
     })
 
   }
 
+  openDialog():void{
+      this.dialog.open(DialogAnimationsExampleDialog,{
+        'width':'400px',
+        data:{message:'hello from pent'}
+      });
+  }
    
 }
