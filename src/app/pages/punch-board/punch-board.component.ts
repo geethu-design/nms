67eb5b9fe673a3dc249d90acb6fa2b2cdfad92b5
geethu <geethukp004@gmail.com>
2025-06-30 00:22:36 +0530

    update routes

--- src/app/pages/punch-board/punch-board.component.ts
+++ src/app/pages/punch-board/punch-board.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PunchBoardService } from '../punch-board/punch-board.service';
 import { punchboardStatusData } from '../punch-board/punch-board.type';
 import { RouterModule } from '@angular/router'; 
 import { CommonModule } from '@angular/common';
+import { PunchboardStatusComponent } from "../punchboard-status/punchboard-status.component";
 @Component({
   selector: 'app-punch-board',
   imports: [
     RouterModule,
-    CommonModule
-
-  ],
+    CommonModule,
+    PunchboardStatusComponent
+],
   templateUrl: './punch-board.component.html',
   styleUrl: './punch-board.component.scss',
   standalone:true
 })
-export class PunchBoardComponent {
+export class PunchBoardComponent implements OnInit {
    constructor(private punchboardService:PunchBoardService){}
+  ngOnInit(): void {
+    
+  }
 }
