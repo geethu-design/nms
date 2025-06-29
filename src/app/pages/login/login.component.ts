e93de078a947690dbc456b10e975b6f21a9db860
geethu <geethukp004@gmail.com>
2025-06-30 00:21:20 +0530

    Add remember me

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EntryComponent } from "../../shared/components/entry/entry.component";
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormGroup,FormBuilder,Validators,ReactiveFormsModule, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common'; 
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ILoginRequest } from './login.type';
 import { LoginService } from '../login/login.service';
-
+ 
 @Component({
   selector: 'app-login',
-  imports: [EntryComponent,
+  imports: [
+    EntryComponent,
     MatInputModule,
     MatButtonModule,
     ReactiveFormsModule,
@@ -35,7 +36,17 @@ export class LoginComponent implements OnInit {
   ){
   }
   ngOnInit(): void {
-   
+   const savedEmail = localStorage.getItem('savedEmail');
+   const savedPassword = localStorage.getItem('savedPassword');
+   const savedRememberMeStr = localStorage.getItem('savedRememberMe');
+   const savedRememberMe = localStorage.getItem('rememberMe') === 'true';
+
+   this.loginForm.setValue({
+    email: savedEmail || '',
+    password: savedPassword || '',
+    rememberMe: savedRememberMe,
+    retryCount: 4
+  });
   }
   getEmailControl(){
     return this.loginForm.controls['email'];
@@ -52,18 +63,29 @@ export class LoginComponent implements OnInit {
   const password = this.loginForm.value.password!;
   const rememberMe = this.loginForm.value.rememberMe;
   const retryCount= 2;
- 
+  console.log('Login attempt:', { email, password, rememberMe });
+  if(rememberMe){
+    localStorage.setItem('savedEmail',email);
+    localStorage.setItem('savedPassword',password);
+    localStorage.setItem('rememberMe','true');
+  }else{
+    console.log('Clear localStorage');
+    localStorage.removeItem('savedEmail');
+    localStorage.removeItem('savedPassword');
+    localStorage.removeItem('rememberMe');
+  }
+
   const payload:ILoginRequest ={
     data:{
       username:email,
       password:password,
       retryCount:retryCount?.toString()
     }
-  }
+  };
     this.loginService.login(payload).subscribe(res=>{
        this.router.navigate(['/sidebar/dashboard']);
-
-    })
+     });
+     console.log('Saving to localStorage:', { email, password, rememberMe });
 
    }
 }
